c5e688ba2cd950555a8ef9e53513202a5bb6d068
Artx Hundiak <ahundiak@gmail.com>
2016-03-22 09:48:33 -0500

    Implemented Schedule Team

---
+++ src/AppBundle/Action/Schedule/ScheduleRepository.php
@@ -0,0 +1,158 @@
+<?php
+namespace AppBundle\Action\Schedule;
+
+use Doctrine\DBAL\Connection;
+
+/** ================================================
+ *  This is basically a view model
+ */
+class ScheduleRepository
+{
+    /** @var  Connection */
+    private $conn;
+
+    public function __construct(Connection $conn)
+    {
+        $this->conn = $conn;
+    }
+    protected function extractFromRow(array $row, $prefix)
+    {
+        $len = strlen($prefix);
+        $item = [];
+        foreach($row as $key => $value) {
+            if (substr($key,0,$len) === $prefix) {
+                $item[substr($key,$len)] = $value;
+            }
+        }
+        return $item;
+    }
+    public function findProjectTeams()
+    {
+        $qb = $this->conn->createQueryBuilder();
+
+        $qb->addSelect([
+            'project_team.id         AS  id',
+            'project_team.keyx       AS `key`',
+            'project_team.name       AS  name',
+            'project_team.projectKey AS  project_key',
+            'project_team.levelKey   AS  level_key',
+        ]);
+        $qb->from('teams','project_team');
+
+        $qb->andWhere('project_team.levelKey LIKE :level');
+        $qb->andWhere("project_team.status = 'Active'");
+
+        $qb->setParameter(':level','%_Core');
+
+        $qb->addOrderBy('project_team.levelKey','DESC');
+
+        $stmt = $qb->execute();
+        $projectTeams = [];
+        while($projectTeam = $stmt->fetch()) {
+
+            $levelParts = explode('_',$projectTeam['level_key']);
+            $projectTeam['div'] = $levelParts[1];
+
+            $projectTeams[] = $projectTeam;
+        }
+        return $projectTeams;
+    }
+    // Move to game repo
+    public function findProjectGamesForProjectTeamKeys(array $projectTeamKeys)
+    {
+        if (count($projectTeamKeys) < 1) return [];
+
+        $qb = $this->conn->createQueryBuilder();
+
+        $qb->addSelect('distinct project_game.id AS id');
+
+        $qb->from('games','project_game');
+
+        $qb->leftJoin(
+            'project_game',
+            'game_teams',
+            'project_game_team',
+            'project_game_team.gameId = project_game.id'
+        );
+        $qb->andWhere('project_game_team.teamKey IN (?)');
+
+        $stmt = $this->conn->executeQuery($qb->getSQL(),[$projectTeamKeys],[Connection::PARAM_STR_ARRAY]);
+
+        $projectGameIds = [];
+        while($row = $stmt->fetch()) {
+            $projectGameIds[] = $row['id'];
+        }
+        return $this->findProjectGamesForProjectGamesIds($projectGameIds);
+    }
+    protected function findProjectGamesForProjectGamesIds(array $projectGameIds)
+    {
+        if (count($projectGameIds) < 1) return [];
+
+        $qb = $this->conn->createQueryBuilder();
+
+        $qb->addSelect([
+            'project_game.id         AS id',
+            'project_game.num        AS number',
+            'project_game.dtBeg      AS start',
+            'project_game.fieldName  AS field_name',
+            'project_game.groupType  AS group_type',
+            'project_game.groupName  AS group_name',
+            'project_game.levelKey   AS level_key',
+            'project_game.projectKey AS project_key',
+            'project_game.status     AS status',
+        ]);
+        $qb->from('games','project_game');
+
+        $qb->addOrderBy('project_game.dtBeg',   'ASC');
+        $qb->addOrderBy('project_game.levelKey','ASC');
+        $qb->addOrderBy('project_game.num',     'ASC');
+
+        $qb->andWhere('project_game.id IN (?)');
+
+        $stmt = $this->conn->executeQuery($qb->getSQL(),[$projectGameIds],[Connection::PARAM_INT_ARRAY]);
+
+        $projectGames = [];
+        while($projectGame = $stmt->fetch()) {
+
+            $start = \DateTime::createFromFormat('Y-m-d H:i:s',$projectGame['start']);
+
+            $projectGame['dow']  = $start->format('D');
+            $projectGame['time'] = $start->format('g:i A');
+
+            $levelParts = explode('_',$projectGame['level_key']);
+            $projectGame['group'] = sprintf('%s %s %s %s',
+                $levelParts[1],
+                $levelParts[2],
+                $projectGame['group_type'],
+                $projectGame['group_name']
+            );
+            $projectGame['project_game_teams'] = [];
+
+            $projectGames[$projectGame['id']] = $projectGame;
+        }
+        // Add teams
+        $qb = $this->conn->createQueryBuilder();
+
+        $qb->addSelect([
+            'project_game_team.id         AS  id',
+            'project_game_team.teamKey    AS `key`',
+            'project_game_team.slot       AS  slot',
+            'project_game_team.teamName   AS  name',
+            'project_game_team.groupSlot  AS  group_slot',
+            'project_game_team.gameId     AS  project_game_id',
+        ]);
+        $qb->from('game_teams','project_game_team');
+
+        $qb->addOrderBy('project_game_id','ASC');
+        $qb->addOrderBy('slot',           'ASC');
+
+        $qb->andWhere('project_game_team.gameId IN (?)');
+
+        $stmt = $this->conn->executeQuery($qb->getSQL(),[$projectGameIds],[Connection::PARAM_INT_ARRAY]);
+
+        while($projectGameTeam = $stmt->fetch()) {
+            $projectGames[$projectGameTeam['project_game_id']]['project_game_teams'][$projectGameTeam['slot']] = $projectGameTeam;
+        }
+        return $projectGames;
+    }
+}
---
+++ src/AppBundle/Action/Schedule/Team/ScheduleTeamController.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace AppBundle\Action\Schedule\Team;
+
+use AppBundle\Action\AbstractController;
+
+use AppBundle\Action\Schedule\ScheduleRepository;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ScheduleTeamController extends AbstractController
+{
+    /** @var  ScheduleRepository */
+    private $scheduleRepository;
+
+    private $projectTeams;
+    private $projectTeamKeys = [];
+
+    private $projectGames = [];
+
+    public function __construct(ScheduleRepository $scheduleRepository)
+    {
+        $this->scheduleRepository = $scheduleRepository;
+    }
+    public function __invoke(Request $request)
+    {
+        // Save selected teams in session
+        $session = $request->getSession();
+        $this->projectTeamKeys = $session->has('project_team_keys') ? $session->get('project_team_keys') : [];
+
+        // Search posted
+        if ($request->isMethod('POST')) {
+            $this->projectTeamKeys = $request->request->get('project_teams');
+            $session->set('project_team_keys',$this->projectTeamKeys);
+        }
+
+        // Find games
+        $this->projectGames = $this->scheduleRepository->findProjectGamesForProjectTeamKeys($this->projectTeamKeys);
+
+        // Search form
+        $this->projectTeams = $this->scheduleRepository->findProjectTeams();
+
+        return new Response($this->renderPage());
+    }
+    protected function renderPage()
+    {
+        $projectTeamCount = count($this->projectTeams);
+
+        /** =================
+         * Note: Using a table here because at one point we supported multiple programs
+         */
+
+        $content = <<<EOD
+<div id="layout-block">
+  <form class="cerad_common_form1" method="post" action="{$this->generateUrl('app_schedule_team')}">
+  <fieldset>
+    <table>
+      <tr><th>Core Teams({$projectTeamCount})</th></tr>
+      <tr><td>
+        <select name="project_teams[]" multiple size="10">
+          <option value=0">Select Teams</option>
+          {$this->renderProjectTeamOptions()}
+        </select>
+      <td><tr>
+    </table>
+    <div class="layout-block">
+      <div class="controls"><button type="submit" id="form_search" name="search" class="submit">Search</button></div>
+      <div style="padding: 10px 0 0 25%; float: left;"><a href="/project/natgames/schedule-team.xls">Export to Excel</a></div>
+      <div style="padding: 10px 0 0 4em; float: left;"><a href="/project/natgames/schedule-team.csv">Export to Text</a></div>
+    </div>
+  </fieldset>
+  </form>
+</div>
+{$this->renderProjectGames()}
+EOD;
+        $baseTemplate = $this->getBaseTemplate();
+        $baseTemplate->setContent($content);
+        return $baseTemplate->render();
+    }
+    private function renderProjectTeamOptions()
+    {
+        $html = null;
+        foreach($this->projectTeams as $projectTeam) {
+
+            $selected = in_array($projectTeam['key'],$this->projectTeamKeys) ? ' selected' : null;
+
+            $html .= sprintf('<option%s value="%s">%s %s</option>' . "\n",
+                $selected,
+                $this->escape($projectTeam['key']),
+                $this->escape($projectTeam['div']),
+                $this->escape($projectTeam['name']));
+        }
+        return $html;
+    }
+    private function renderProjectGamesRows($projectGames)
+    {
+        $html = null;
+        foreach($projectGames as $projectGame) {
+
+            $projectGameTeamHome = $projectGame['project_game_teams'][1];
+            $projectGameTeamAway = $projectGame['project_game_teams'][2];
+
+            $html .= <<<EOD
+<tr id="schedule-team-{$projectGame['number']}" class="game-status-{$projectGame['number']}">
+  <td class="schedule-game" >{$projectGame['number']}</td>
+  <td class="schedule-dow"  >{$projectGame['dow']}</td>
+  <td class="schedule-time" >{$projectGame['time']}</td>
+  <td class="schedule-field">{$projectGame['field_name']}</td>
+  <td class="schedule-group">{$projectGame['group']}</td>
+  <td>&nbsp;</td>
+  <td><table>
+    <tr><td>{$projectGameTeamHome['group_slot']}</td></tr>
+    <tr><td>{$projectGameTeamAway['group_slot']}</td></tr>
+  </table></td>
+  <td><table>
+    <tr><td class="text-left">{$projectGameTeamHome['name']}</td></tr>
+    <tr><td class="text-left">{$projectGameTeamAway['name']}</td></tr>
+  </table></td>
+</tr>
+EOD;
+        }
+        return $html;
+    }
+    private function renderProjectGames()
+    {
+        $projectGames = $this->scheduleRepository->findProjectGamesForProjectTeamKeys($this->projectTeamKeys);
+        $projectGameCount = count($projectGames);
+
+        return <<<EOD
+<div id="layout-block">
+<div class="schedule-games-list">
+<table id="schedule" class="schedule" border="1">
+  <thead>
+    <tr><th colspan="20" class="text-center">Team Schedule - Game Count: {$projectGameCount}</th></tr>
+    <tr>
+      <th class="schedule-game" >Game</th>
+      <th class="schedule-dow"  >Day</th>
+      <th class="schedule-time" >Time</th>
+      <th class="schedule-field">Field</th>
+      <th class="schedule-group">Group</th>
+      <th class="schedule-blank">&nbsp;</th>
+      <th class="schedule-slot" >Slot</th>
+      <th class="schedule-teams">Home / Away</th>
+    </tr>
+  </thead>
+  <tbody>
+  {$this->renderProjectGamesRows($projectGames)}
+  </tbody>
+</table>
+</div>
+<br />
+</div
+EOD;
+    }
+}
