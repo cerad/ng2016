afbf6a19642a35231f2da570fa361792a5b29fe2
Rick Roberts <ayso1sra@gmail.com>
2016-04-15 07:53:03 -0700

    Extracted StandingsCalculator as service

--- src/AppBundle/Action/Results/Sportsmanship/ResultsSportsmanshipView.php
+++ src/AppBundle/Action/Results/Sportsmanship/ResultsSportsmanshipView.php
@@ -5,7 +5,7 @@ namespace AppBundle\Action\Results\Sportsmanship;
 use AppBundle\Action\AbstractView;
 
 use AppBundle\Action\Schedule\ScheduleRepository;
-use AppBundle\Action\Results\PoolPlay\Calculator\StandingsCalculator;
+use AppBundle\Action\Results\Sportsmanship\Calculator\SportsmanshipCalculator;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,19 +15,19 @@ class ResultsSportsmanshipView extends AbstractView
     /** @var  ScheduleRepository */
     private $scheduleRepository;
 
-    /** @var  StandingsCalculator */
-    private $standingsCalculator;
+    /** @var  SportsmanshipCalculator */
+    private $sportsmanshipCalculator;
 
     private $games;
-    private $standings = array();
+    private $standings;
 
     public function __construct(
         ScheduleRepository  $scheduleRepository,
-        StandingsCalculator $standingsCalculator
+        SportsmanshipCalculator $sportsmanshipCalculator
     )
     {
         $this->scheduleRepository  = $scheduleRepository;
-        $this->standingsCalculator = $standingsCalculator;
+        $this->sportsmanshipCalculator = $sportsmanshipCalculator;
     }
     public function __invoke(Request $request)
     {
@@ -37,82 +37,12 @@ class ResultsSportsmanshipView extends AbstractView
         $criteria = $request->attributes->get('criteria');
           
         $this->games = count($criteria) > 1 ? $this->scheduleRepository->findProjectGames($criteria) : [];
-   
-        $this->computeSportsmanshipStandings();
+ 
+        $this->standings = $this->sportsmanshipCalculator->getSportsmanshipStandings($this->games);
         
         return new Response($this->renderPage());
     }
     
-    // Comparison function
-    // ref: http://php.net/manual/en/function.ksort.php
-    protected function sksort(&$array, $subkey="id", $sort_ascending=false) {
-    
-        if (count($array))
-            $temp_array[key($array)] = array_shift($array);
-    
-        foreach($array as $key => $val){
-            $offset = 0;
-            $found = false;
-            foreach($temp_array as $tmp_key => $tmp_val)
-            {
-                if(!$found and strtolower($val[$subkey]) > strtolower($tmp_val[$subkey]))
-                {
-                    $temp_array = array_merge(    (array)array_slice($temp_array,0,$offset),
-                                                array($key => $val),
-                                                array_slice($temp_array,$offset)
-                                              );
-                    $found = true;
-                }
-                $offset++;
-            }
-            if(!$found) $temp_array = array_merge($temp_array, array($key => $val));
-        }
-    
-        if ($sort_ascending) $array = array_reverse($temp_array);
-    
-        else $array = $temp_array;
-    }
-    
-    protected function computeSportsmanshipStandings(){
-        
-        foreach($this->games as $game) {
-
-            foreach ($game['teams'] as $team) {
-                
-                $name = $team['name'];
-                   
-                $teamReport = $team['report'];
-         
-                $teamSportsmanship = isset($teamReport['sportsmanship']) ? $teamReport['sportsmanship'] : null;
-         
-                if (isset($this->standings[$name])) {
-                    $this->standings[$name][] = $teamSportsmanship;
-                } else {
-                    $this->standings[$name] = array($teamSportsmanship);
-                }
-            }       
-
-        }
-      
-        foreach($this->standings as &$team) {
-
-            $total = 0;
-            $gameCount = 0;
-
-            foreach($team as $sp) {
-                $total += $sp;
-                $gameCount += 1;
-            }
-
-            $team['totalSP'] = $total;
-            $team['gamesPlayed'] = $gameCount;
-            $team['avgSP'] = !empty($gameCount) ? number_format($total / $gameCount,3) : null;
-        }
-        
-        //sort by avg Sportsmanship
-        $this->sksort($this->standings, "avgSP");
-
-    }
     /* ========================================================
      * Render Page
      */
@@ -231,6 +161,10 @@ EOD;
      */
     protected function renderSportsmanship()
     {
+        if (empty($this->games)) {
+            return null;
+        }
+        
         $levelKey = str_replace('_',' ',array_values($this->games)[0]['level_key']);
 
         $html = <<<EOD
