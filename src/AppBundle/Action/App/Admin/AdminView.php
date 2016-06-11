219fecc95d5f1f589ca202f94eee727d6389e987
Rick Roberts <ayso1sra@gmail.com>
2016-06-11 16:51:52 -0700

    144-team_management_functionality - Team Export complete - Updated Admin page to show Game Listing & Export Team links - Added Bootstrap plugin for file input (https://github.com/kartik-v/bootstrap-fileinput) to composer.json - Reordered Game list to put Registered Teams at the top
    Also:
    - Refactored mystery url routes for clarity

--- src/AppBundle/Action/App/Admin/AdminView.php
+++ src/AppBundle/Action/App/Admin/AdminView.php
@@ -125,8 +125,8 @@ EOT;
   </div>
   <div class="panel-body">
     <ul>
-      <li><a href="#">View Teams</a></li>
-      <li><a href="#">Export Teams (Excel)</a></li>
+      <li><a href="{$this->generateUrl('game_listing')}">View Teams</a></li>
+      <li><a href="{$this->generateUrl('regteam_2016', ['_format' => 'xls'])}">Export Teams (Excel)</a></li>
 EOT;
       if ($this->isGranted('ROLE_ADMIN')) {
         $html .= <<<EOT
