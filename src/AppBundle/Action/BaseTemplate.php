b765445452cb24a2851c6e0d6948ce3e275e59ca
Rick Roberts <ayso1sra@gmail.com>
2016-06-15 17:51:25 -0700

    173-review_top_menus - style ScheduleSearchForm for mobile - update styling on MyPlans - consolidate top menus

--- src/AppBundle/Action/BaseTemplate.php
+++ src/AppBundle/Action/BaseTemplate.php
@@ -277,17 +277,25 @@ EOT;
         if (!$this->showResultsMenu) {
             return null;
         }
-        return
-<<<EOT
+        $html = <<<EOT
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">REFEREES <span class="caret"></span></a>
          <ul class="dropdown-menu">
-            <li><a href="{$this->generateUrl('schedule_my_2016')}">MY SCHEDULE</a></li>
             <li><a href="{$this->generateUrl('schedule_official_2016')}">REQUEST ASSIGNMENTS</a></li>
-            <li><a href="{$this->generateUrl('schedule_official_2016')}">REFEREE SCHEDULES</a></li>
+EOT;
+
+        if ($this->isGranted('ROLE_ASSIGNOR')) {
+            $html .= <<<EOT
+            <li><a href="{$this->generateUrl('schedule_assignor_2016')}">ASSIGNOR SCHEDULE</a></li>
+EOT;
+        }
+
+        $html .= <<<EOT
          </ul>
        </li>
 EOT;
+        
+        return $html;
     }
 
     protected function renderMyAccount()
@@ -300,9 +308,11 @@ EOT;
         <li class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">MY STUFF<span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li><a href="{$this->generateUrl('project_person_update')}">MY PLANS</a></li>
-          <li><a href="{$this->generateUrl('app_home')}">MY AYSO INFO</a></li>
-          <li><a href="{$this->generateUrl('app_home')}">MY SCHEDULE</a></li>
+          <li><a href="{$this->generateUrl('app_home')}">MY INFO</a></li>
+          <li><a href="{$this->generateUrl('project_person_update')}">MY PLANS & AVAILABILITY</a></li>
+          <li><a href="{$this->generateUrl('schedule_my_2016')}">MY SCHEDULE</a></li>
+          <li><a href="{$this->generateUrl('reg_person_persons_update')}">MY CREW</a></li>
+          <li><a href="{$this->generateUrl('reg_person_teams_update')}">MY TEAMS</a></li>
         </ul>
 EOT;
     }
