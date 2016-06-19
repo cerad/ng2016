d1abb14d9888f49485d194d9c0ed31e6236ae8ec
Art (giles) Hundiak <ahundiak@gmail.com>
2016-06-19 09:46:24 -0500

    ImportPoolTeams skeleton.

---
+++ src/AppBundle/Action/PoolTeam/Import/PoolTeamImportReaderExcel.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace AppBundle\Action\PoolTeam\Import;
+
+use AppBundle\Common\ExcelReaderTrait;
+
+class PoolTeamImportReaderExcel
+{
+    use ExcelReaderTrait;
+
+    private $poolTeams = [];
+
+    protected function processRow($row)
+    {
+        $colProjectId       = 0;
+        $colGameNumber      = 1;
+        $colDate            = 2;
+        $colTime            = 3;
+        $colFieldName       = 4;
+        $colHomeTeamPoolKey = 5;
+        $colHomeTeamName    = 6;
+        $colAwayTeamName    = 7;
+        $colAwayTeamPoolKey = 8;
+
+        // Skip empty lines
+        $projectId = trim($row[$colProjectId]);
+        if (!$projectId) return;
+        
+        $gameNumber = (integer)trim($row[$colGameNumber]);
+        if (!$gameNumber) return null;
+
+        $date = $this->processDate($row[$colDate]);
+        $time = $this->processTime($row[$colTime]);
+
+        $homePoolTeamKey = trim($row[$colHomeTeamPoolKey]);
+        $awayPoolTeamKey = trim($row[$colAwayTeamPoolKey]);
+
+        $game = [
+            'projectId'       => $projectId,
+            'gameNumber'      => $gameNumber,
+            'gameId'          => $projectId . ':' . abs($gameNumber),
+            'date'            => $date,
+            'time'            => $time,
+            'start'           => $date . ' ' . $time,
+            'fieldName'       => trim($row[$colFieldName]),
+            'homePoolTeamKey' => $homePoolTeamKey,
+            'awayPoolTeamKey' => $awayPoolTeamKey,
+            'homePoolTeamId'  => $projectId . ':' . $homePoolTeamKey,
+            'awayPoolTeamId'  => $projectId . ':' . $awayPoolTeamKey,
+            'homeTeamName'    => trim($row[$colHomeTeamName]),
+            'awayTeamName'    => trim($row[$colAwayTeamName]),
+        ];
+        $this->games[] = $game;
+    }
+    public function read($filename)
+    {
+        // Tosses exception
+        $reader = \PHPExcel_IOFactory::createReaderForFile($filename);
+        
+        // Need this otherwise dates and such are returned formatted
+        /** @noinspection PhpUndefinedMethodInspection */
+        $reader->setReadDataOnly(true);
+
+        // Just grab all the rows
+        $wb = $reader->load($filename);
+        $ws = $wb->getSheet(0);
+        $rows = $ws->toArray();
+        array_shift($rows); // Discard header line
+    
+        foreach($rows as $row) {
+            //$this->processRow($row);
+        }
+        return $this->poolTeams;
+    }
+}
---
+++ src/AppBundle/Action/PoolTeam/Import/PoolTeamImportResults.php
@@ -0,0 +1,41 @@
+<?php
+namespace AppBundle\Action\PoolTeam\Import;
+
+class PoolTeamImportResults
+{
+    public $commit;
+    public $fileName;
+    
+    public $totalCount   = 0;
+    public $createdCount = 0;
+    public $deletedCount = 0;
+    public $updatedCount = 0;
+    
+    public $totalPoolTeams = [];
+    public $createdPoolTeams = [];
+    public $deletedPoolTeams = [];
+    public $updatedPoolTeams = [];
+    
+    public $invalidPoolTeamIds = [];
+    public $invalidPoolTeamIdsCount;
+    
+    public function __construct($poolTeams,$commit,$fileName)
+    {
+        $this->totalPoolTeams = $poolTeams;
+        $this->commit         = $commit;
+        $this->fileName       = $fileName;
+    }
+    public function addUpdatedPoolTeam($poolTeam)
+    {
+        $this->updatedPoolTeams[$poolTeam['poolTeamId']] = $poolTeam;
+    }
+    public function calcCounts()
+    {
+        $this->totalCount   = count($this->totalPoolTeams);
+        $this->createdCount = count($this->createdPoolTeams);
+        $this->deletedCount = count($this->deletedPoolTeams);
+        $this->updatedCount = count($this->updatedPoolTeams);
+        
+        $this->invalidPoolTeamIdsCount = count($this->invalidPoolTeamIds);
+    }
+}
