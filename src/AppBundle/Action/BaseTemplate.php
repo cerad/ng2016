52aa460842ebd7413d47e6639d0be8b2677455f1
Rick Roberts <web.ayso@gmail.com>
2017-07-05 21:57:31 -0700

    Full redirect to andy

--- src/AppBundle/Action/BaseTemplate.php
+++ src/AppBundle/Action/BaseTemplate.php
@@ -30,17 +30,6 @@ class BaseTemplate extends AbstractTemplate
     {
         return <<<EOT
         {$this->renderHead()}
-        {$this->renderHeader()}
-        <body>
-            <div id="layout-topmenu">
-              {$this->renderTopMenu()}
-            </div>
-            <div class="container">
-              {$this->content}
-            </div>
-      {$this->renderFooter()}
-      
-    {$this->renderScripts()}
 
 EOT;
     }
@@ -53,18 +42,6 @@ EOT;
 <html lang="en">
 <head>
     <meta http-equiv="refresh" content="0; url=http://aysonationalopencup.org/contact-ayso/">
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{$this->escape($this->project['abbv'])}</title>
-    <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/images/favicon.ico">
-    <link rel="apple-touch-icon" type="image/png" href="/images/apple-touch-icon-72x72.png"><!-- iPad -->
-    <link rel="apple-touch-icon" type="image/png" sizes="114x114" href="/images/apple-touch-icon-114x114.png"><!-- iPhone4 -->
-    <link rel="icon" type="image/png" href="/images/apple-touch-icon-114x114.png"><!-- Opera Speed Dial, at least 144?114 px -->
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.1.1/normalize.min.css" media="all" />
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="/css/bootstrap.vertical-tabs.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.3.2/css/fileinput.min.css" media="all" rel="stylesheet" />
-    <link rel="stylesheet" type="text/css" href="/css/zayso.css" media="all" />
 </head>
 EOT;
     }
