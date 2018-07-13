b4436c3c9bff43e0a8444cfec361567fe56c7e7e
Rick Roberts <web.ayso@gmail.com>
2018-07-13 12:55:27 -0700

    Version 2018.07.13.11:  - Added Club Divisions to game listing Div selector

--- src/AppBundle/Action/Game/Listing/GameListingSearchForm.php
+++ src/AppBundle/Action/Game/Listing/GameListingSearchForm.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace AppBundle\Action\Game\Listing;
 
 use AppBundle\Action\AbstractForm;
@@ -14,37 +15,45 @@ class GameListingSearchForm extends AbstractForm
 
     public function __construct(
         $projectChoices,
-        $projects, Connection $conn
+        $projects,
+        Connection $conn
     ) {
-        $this->projects       = $projects;
+        $this->projects = $projects;
         $this->projectChoices = $projectChoices;
-        $this->conn           = $conn;
+        $this->conn = $conn;
     }
+
     public function handleRequest(Request $request)
     {
-        if (!$request->isMethod('POST')) return;
-        
+        if (!$request->isMethod('POST')) {
+            return;
+        }
+
         $this->isPost = true;
-        
+
         $data = $request->request->all();
         $errors = [];
 
-        $this->formData = array_replace($this->formData,[
-            'projectId'   => $this->filterScalarString($data,'projectId'),
-            'program'     => $this->filterScalarString($data,'program'),
-            'division'    => $this->filterScalarString($data,'division'),
-            'show'        => $this->filterScalarString($data,'show'),
-        ]);
+        $this->formData = array_replace(
+            $this->formData,
+            [
+                'projectId' => $this->filterScalarString($data, 'projectId'),
+                'program' => $this->filterScalarString($data, 'program'),
+                'division' => $this->filterScalarString($data, 'division'),
+                'show' => $this->filterScalarString($data, 'show'),
+            ]
+        );
         $this->formDataErrors = $errors;
     }
+
     public function render()
     {
-        $show      = $this->formData['show'];
-        $program   = $this->formData['program'];
+        $show = $this->formData['show'];
+        $program = $this->formData['program'];
         $projectId = $this->formData['projectId'];
-        $division  = $this->formData['division'];
+        $division = $this->formData['division'];
 
-        $project   = $this->projects[$projectId];
+//        $project = $this->projects[$projectId];
 
         $csrfToken = 'TODO';
 
@@ -53,19 +62,19 @@ class GameListingSearchForm extends AbstractForm
 <form role="form" class="form-inline" style="width: 1200px;" action="{$this->generateUrl($this->getCurrentRouteName())}" method="post">
   <div class="form-group">
     <label for="projectId">Project</label>
-    {$this->renderInputSelect($this->projectChoices,$projectId,'projectId')}
+    {$this->renderInputSelect($this->projectChoices, $projectId, 'projectId')}
   </div>
   <div class="form-group">
     <label for="program">Program</label>
-    {$this->renderInputSelect($this->programChoices,$program,'program')}
+    {$this->renderInputSelect($this->programChoices, $program, 'program')}
   </div>
   <div class="form-group">
     <label for="division">Div</label>
-    {$this->renderInputSelect($this->divisionChoices,$division,'division')}
+    {$this->renderInputSelect($this->divisionChoices($program), $division, 'division')}
   </div>
   <div class="form-group">
     <label for="show">Show</label>
-    {$this->renderInputSelect($this->showChoices,$show,'show')}
+    {$this->renderInputSelect($this->showChoices, $show, 'show')}
   </div>
   <input type="hidden" name="_csrf_token" value="{$csrfToken}" />
   <button type="submit" class="btn btn-sm btn-primary submit">
@@ -75,35 +84,60 @@ class GameListingSearchForm extends AbstractForm
 </form>
 <br/>
 EOD;
+
         return $html;
     }
+
     private $showChoices = [
-        'all'         => 'All',
-        'regTeams'    => 'Registered Teams',
-        'poolTeams'   => 'Pool Teams',
-        'games'       => 'Games',
+        'all' => 'All',
+        'regTeams' => 'Registered Teams',
+        'poolTeams' => 'Pool Teams',
+        'games' => 'Games',
         'gameNumbers' => 'Game Numbers',
     ];
     private $programChoices = [
-        null        => 'All',
-        'Core'    => 'Core',
-        'Club'    => 'Club',
+        null => 'All',
+        'Core' => 'Core',
+        'Club' => 'Club',
     ];
-    private $divisionChoices = [
-         null  => 'All',
-        'B10U' => 'Boys 10U',
-        'G10U' => 'Girls 10U',
-        'B11U' => 'Boys 11U',
-        'G11U' => 'Girls 11U',
-        'B12U' => 'Boys 12U',
-        'G12U' => 'Girls 12U',
-        'B13U' => 'Boys 13U',
-        'G13U' => 'Girls 13U',
-        'B14U' => 'Boys 14U',
-        'G14U' => 'Girls 14U',
-        'B16U' => 'Boys 16U',
-        'G16U' => 'Girls 16U',
-        'B19U' => 'Boys 19U',
-        'G19U' => 'Girls 19U',
-    ];
+
+    private function divisionChoices($program)
+    {
+        $program = is_null($program)? 'All': $program;
+
+        $divisionChoices = [
+            null => 'All',
+        ];
+        if ($program == 'All' || $program == 'Core') {
+            $divisionChoices = array_merge(
+                $divisionChoices,
+                [
+                    'B10U' => 'Boys 10U',
+                    'G10U' => 'Girls 10U',
+                    'B12U' => 'Boys 12U',
+                    'G12U' => 'Girls 12U',
+                    'B14U' => 'Boys 14U',
+                    'G14U' => 'Girls 14U',
+                    'B19U' => 'Boys 19U',
+                    'G19U' => 'Girls 19U',
+                ]
+            );
+        }
+        if ($program == 'All' || $program == 'Club') {
+            $divisionChoices = array_merge(
+                $divisionChoices,
+                [
+                    'B2004' => 'Boys 2004',
+                    'B2006' => 'Boys 2006',
+                    'B2007' => 'Boys 2007',
+                    'B2008' => 'Boys 2008',
+                    'G2003/2004' => 'Girls 2003/2004',
+                    'G2005/2006' => 'Girls 2005/2006',
+                    'G2007' => 'Girls 2007',
+                ]
+            );
+        }
+
+        return $divisionChoices;
+    }
 }
