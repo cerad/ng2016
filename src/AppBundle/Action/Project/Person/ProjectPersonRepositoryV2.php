10499d4fa623252344f718b699bd6b051ee188bf
Rick Roberts <ayso1sra@gmail.com>
2019-04-04 15:34:34 -0700

    Reformat for comparison

--- src/AppBundle/Action/Project/Person/ProjectPersonRepositoryV2.php
+++ src/AppBundle/Action/Project/Person/ProjectPersonRepositoryV2.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace AppBundle\Action\Project\Person;
 
 use AppBundle\Action\Services\VolCerts;
@@ -34,7 +35,7 @@ class ProjectPersonRepositoryV2
         // Grab the persons, TODO use a query builder object here
         $sql = 'SELECT * FROM projectPersons WHERE projectKey = ? ';
         if ($name) {
-            $params[] = '%' . $name . '%';
+            $params[] = '%'.$name.'%';
             $sql .= ' AND name LIKE ?';
         }
         if ($registered !== null) {
@@ -47,7 +48,7 @@ class ProjectPersonRepositoryV2
         }
         $sql .= ' ORDER BY name';
 
-        $stmt = $this->conn->executeQuery($sql,$params);
+        $stmt = $this->conn->executeQuery($sql, $params);
         $personRows = [];
         $fedKeys = [];
         while ($personRow = $stmt->fetch()) {
@@ -83,18 +84,18 @@ class ProjectPersonRepositoryV2
         // Merge roles
         $personIds = array_keys($personRows);
         $sql = 'SELECT * from projectPersonRoles WHERE projectPersonId IN (?)';
-        $stmt = $this->conn->executeQuery($sql,[$personIds],[Connection::PARAM_INT_ARRAY]);
-        while($roleRow = $stmt->fetch()) {
+        $stmt = $this->conn->executeQuery($sql, [$personIds], [Connection::PARAM_INT_ARRAY]);
+        while ($roleRow = $stmt->fetch()) {
             $personRows[$roleRow['projectPersonId']]['roles'][$roleRow['role']] = $roleRow;
         }
 
         // Make objects
         $persons = [];
-        foreach($personRows as $personRow)
-        {
+        foreach ($personRows as $personRow) {
             $person = new ProjectPerson();
             $persons[] = $person->fromArray($personRow);
         }
+
         return $persons;
     }
 
@@ -104,24 +105,27 @@ class ProjectPersonRepositoryV2
      * @return ProjectPerson|null
      * @throws DBALException
      */
-    public function find($projectKey,$personKey)
+    public function find($projectKey, $personKey)
     {
         $sql = 'SELECT * FROM projectPersons WHERE projectKey = ? AND personKey = ?';
-        $stmt = $this->conn->executeQuery($sql,[$projectKey,$personKey]);
-        
+        $stmt = $this->conn->executeQuery($sql, [$projectKey, $personKey]);
+
         $row = $stmt->fetch();
-        if (!$row) return null;
+        if (!$row) {
+            return null;
+        }
 
         $row['plans'] = isset($row['plans']) ? unserialize($row['plans']) : null;
         $row['avail'] = isset($row['avail']) ? unserialize($row['avail']) : null;
-        
+
         $row['roles'] = [];
         $sql = 'SELECT * from projectPersonRoles WHERE projectPersonId = ?';
-        $stmt = $this->conn->executeQuery($sql,[$row['id']]);
-        while($roleRow = $stmt->fetch()) {
+        $stmt = $this->conn->executeQuery($sql, [$row['id']]);
+        while ($roleRow = $stmt->fetch()) {
             $row['roles'][$roleRow['role']] = $roleRow;
         }
         $person = new ProjectPerson();
+
         return $person->fromArray($row);
     }
 
@@ -133,32 +137,33 @@ class ProjectPersonRepositoryV2
     public function save(ProjectPerson $person)
     {
         $row = $person->toArray();
-        
+
         $row['plans'] = isset($row['plans']) ? serialize($row['plans']) : null;
         $row['avail'] = isset($row['avail']) ? serialize($row['avail']) : null;
 
-        $id = $row['id']; unset($row['id']);
+        $id = $row['id'];
+        unset($row['id']);
 
-        $roles = $row['roles']; unset($row['roles']);
+        $roles = $row['roles'];
+        unset($row['roles']);
 
         if ($id) {
-            $this->conn->update('projectPersons',$row,['id' => $id]);
+            $this->conn->update('projectPersons', $row, ['id' => $id]);
             $row['id'] = $id;
-        }
-        else {
+        } else {
             // Consider it to be a trigger
-            $row['name'] = $this->generateUniqueName($row['projectKey'],$row['name']);
+            $row['name'] = $this->generateUniqueName($row['projectKey'], $row['name']);
             $this->conn->insert('projectPersons', $row);
             $row['id'] = $this->conn->lastInsertId();
         }
         $row['roles'] = [];
-        foreach($roles as $roleKey => $personRole)
-        {
+        foreach ($roles as $roleKey => $personRole) {
             $personRole['projectPersonId'] = $row['id'];
             $row['roles'][$roleKey] = $this->saveRoleArray($personRole);
         }
-        
+
         $person = new ProjectPerson();
+
         return $person->fromArray($row);
     }
 
@@ -176,13 +181,13 @@ class ProjectPersonRepositoryV2
         $id = $row['id'];
         unset($row['id']);
         if ($id) {
-            $this->conn->update('projectPersonRoles',$row,['id' => $id]);
+            $this->conn->update('projectPersonRoles', $row, ['id' => $id]);
             $row['id'] = $id;
-        }
-        else {
+        } else {
             $this->conn->insert('projectPersonRoles', $row);
             $row['id'] = $this->conn->lastInsertId();
         }
+
         return $row;
     }
 
@@ -193,16 +198,18 @@ class ProjectPersonRepositoryV2
      * @param $email
      * @return ProjectPerson
      */
-    public function create($projectKey,$personKey,$name,$email)
+    public function create($projectKey, $personKey, $name, $email)
     {
         $person = new ProjectPerson();
 
-        return $person->fromArray([
-            'projectKey' => $projectKey,
-            'personKey'  => $personKey,
-            'name'       => $name,
-            'email'      => $email,
-        ]);
+        return $person->fromArray(
+            [
+                'projectKey' => $projectKey,
+                'personKey' => $personKey,
+                'name' => $name,
+                'email' => $email,
+            ]
+        );
     }
 
     /**
@@ -210,14 +217,16 @@ class ProjectPersonRepositoryV2
      * @param $badge
      * @return ProjectPersonRole
      */
-    public function createRole($role,$badge)
+    public function createRole($role, $badge)
     {
         $personRole = new ProjectPersonRole();
 
-        return $personRole->fromArray([
-            'role'  => $role,
-            'badge' => $badge,
-        ]);
+        return $personRole->fromArray(
+            [
+                'role' => $role,
+                'badge' => $badge,
+            ]
+        );
     }
 
     /**
@@ -226,21 +235,22 @@ class ProjectPersonRepositoryV2
      * @return null
      * @throws DBALException
      */
-    public function generateUniqueName($projectKey,$name)
+    public function generateUniqueName($projectKey, $name)
     {
         $sql = 'SELECT id FROM projectPersons WHERE projectKey = ? AND name = ?';
         $stmt = $this->conn->prepare($sql);
 
         $cnt = 1;
         $nameTry = $name;
-        while(true) {
-            $stmt->execute([$projectKey,$nameTry]);
+        while (true) {
+            $stmt->execute([$projectKey, $nameTry]);
             if (!$stmt->fetch()) {
-                return($nameTry);
+                return ($nameTry);
             }
             $cnt++;
-            $nameTry = sprintf('%s(%d)',$name,$cnt);
+            $nameTry = sprintf('%s(%d)', $name, $cnt);
         }
+
         return null;
     }
 }
