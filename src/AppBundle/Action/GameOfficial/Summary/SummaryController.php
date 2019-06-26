47f94770f9366231739623f42656743419a18463
Rick Roberts <ayso1sra@gmail.com>
2019-06-26 11:11:34 -0700

    Version 2019.06.26.21:  - Replace projectId constant with injected parameter

--- src/AppBundle/Action/GameOfficial/Summary/SummaryController.php
+++ src/AppBundle/Action/GameOfficial/Summary/SummaryController.php
@@ -10,22 +10,25 @@ use Symfony\Component\HttpFoundation\Request;
 
 class SummaryController extends AbstractController2
 {
+    private $projectId;
     private $gameFinder;
     private $regPersonFinder;
 
     public function __construct(
+        string  $projectId,
         GameFinder      $gameFinder,
         RegPersonFinder $regPersonFinder
     ) {
+        $this->projectId = $projectId;
         $this->gameFinder =      $gameFinder;
         $this->regPersonFinder = $regPersonFinder;
     }
-    public function __invoke(Request $request, $projectId, $program)
+    public function __invoke(Request $request, $program)
     {
         
         // Grab entire games database
         $criteria = [
-            'projectIds'    => [$projectId],
+            'projectIds'    => [$this->projectId],
             'programs'      => [$program],
             'wantOfficials' => true]
         ;
@@ -33,7 +36,7 @@ class SummaryController extends AbstractController2
         $request->attributes->set('games',$games);
 
         // Grab the reg person database and filter for referees
-        $regPersons = $this->regPersonFinder->findRegPersons($projectId);
+        $regPersons = $this->regPersonFinder->findRegPersons($this->projectId);
         $request->attributes->set('regPersons',$regPersons);
 
         //echo sprintf("Game Count: %d, Reg Person Count: %d.\n",count($games),count($regPersons));
