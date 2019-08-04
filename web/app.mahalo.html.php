4cb7640184f7f4e22e8453cd783446d8ff5920fe
Rick Roberts <ayso1sra@gmail.com>
2019-08-04 13:18:51 -0700

    Added thank you static page

---
+++ web/app.mahalo.html.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<!-- saved from url=(0036)https://ng2019.vhx.cloud/maintenance -->
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>NG2019</title>
+    <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/images/favicon.ico">
+    <link rel="apple-touch-icon" type="image/png" href="/images/apple-touch-icon-72x72.png"><!-- iPad -->
+    <link rel="apple-touch-icon" type="image/png" sizes="114x114" href="/images/apple-touch-icon-114x114.png"><!-- iPhone4 -->
+    <link rel="icon" type="image/png" href="/images/apple-touch-icon-114x114.png"><!-- Opera Speed Dial, at least 144?114 px -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.1.1/normalize.min.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.vertical-tabs.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.3.2/css/fileinput.min.css" media="all" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="/css/zayso.css" media="all" />
+</head>
+<body>
+<div class="skBanners">
+    <a href="https://www.aysonationalgames.org/" target="_blank"><img class="width-90"
+                                                                      src="https://ng2019.zayso.org/images/header-ipad_01.png"></a>
+    <center class="skFont  width-90">AYSO WELCOMES YOU TO WAIPIO PENINSULA SOCCER COMPLEX, WAIPAHU, HAWAII, June 30 -
+        July 7, 2019
+    </center>
+</div>
+
+<div id="layout-topmenu">
+    <nav class="navbar navbar-default">
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div id="topmenu" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+
+                <li>
+                    <a href="https://aysonationalgames.org/game-results/" target="_blank">RESULTS &amp; STANDINGS</a>
+                </li>
+
+            </ul>
+        </div><!-- navbar-collapse -->
+
+    </nav>
+</div>
+
+<div class="container">
+    <div style="text-align: center;margin: 40px 0">
+        <h1 class="text-center" style="padding-top:20px; font-style:italic; font-size:1.5em;">
+            <emphasis>MAHALO for participating in the AYSO 2019 National Games!
+            </emphasis>
+        </h1>
+    </div>
+</div>
+<div class="cerad-footer">
+    <br>
+    <hr>
+    <p>For assistance contact Rob McCarthy at
+        <a href="mailto:soccer.ref62@yahoo.com?subject=NG2019%20zAYSO%20question...">soccer.ref62@yahoo.com</a>
+        or 808-286-9280 </p>
+    <p>Version 2019.06.06.00</p>
+</div>
+
+</body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+        integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.3.2/js/fileinput.min.js"></script>
+</html>
